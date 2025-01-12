69113638ba701a1a33a6dee511f7bc5f12b11843
Ryan <git@hexa.mozmail.com>
2025-01-12 12:52:09 -0500

    fix thread logic

--- inky-mbta-tracker/main.py
+++ inky-mbta-tracker/main.py
@@ -2,7 +2,7 @@ import logging
 import os
 import threading
 import time
-from asyncio import CancelledError, Runner, Task, TaskGroup, sleep
+from asyncio import Runner, sleep
 from datetime import UTC, datetime, timedelta
 from queue import Queue
 from random import randint
@@ -10,7 +10,7 @@ from typing import Optional
 
 from config import StopSetup, load_config
 from dotenv import load_dotenv
-from mbta_client import watch_static_schedule, watch_station
+from mbta_client import thread_runner
 from prometheus_client import start_http_server
 from schedule_tracker import ScheduleEvent, process_queue
 
@@ -27,12 +27,15 @@ MAX_TASK_RESTART_MINS = 120
 
 
 class TaskTracker:
-    task: Task
+    task: threading.Thread
     expiration_time: Optional[datetime]
     stop: Optional[StopSetup]
 
     def __init__(
-        self, task: Task, expiration_time: Optional[datetime], stop: Optional[StopSetup]
+        self,
+        task: threading.Thread,
+        expiration_time: Optional[datetime],
+        stop: Optional[StopSetup],
     ):
         self.task = task
         self.expiration_time = expiration_time
@@ -46,88 +49,70 @@ def queue_watcher(queue: Queue[ScheduleEvent]):
         time.sleep(10)
 
 
+# launches a departures tracking thread, target should either be "schedule" or "predictions"
+# returns a TaskTracker
+def start_thread(target: str, stop: StopSetup, queue: Queue[ScheduleEvent]):
+    match target:
+        case "schedule":
+            exp_time = datetime.now().astimezone(UTC) + timedelta(days=365)
+            thr = threading.Thread(
+                target=thread_runner,
+                args=[
+                    target,
+                    stop.stop_id,
+                    stop.route_filter,
+                    stop.direction_filter,
+                    queue,
+                    stop.transit_time_min,
+                    exp_time,
+                ],
+            )
+            thr.start()
+            return TaskTracker(task=thr, expiration_time=exp_time, stop=stop)
+        case "predictions":
+            exp_time = datetime.now().astimezone(UTC) + timedelta(
+                minutes=randint(MIN_TASK_RESTART_MINS, MAX_TASK_RESTART_MINS)
+            )
+            thr = threading.Thread(
+                target=thread_runner,
+                args=[
+                    target,
+                    stop.stop_id,
+                    stop.route_filter,
+                    stop.direction_filter,
+                    queue,
+                    stop.transit_time_min,
+                    exp_time,
+                ],
+            )
+            thr.start()
+            return TaskTracker(task=thr, expiration_time=exp_time, stop=stop)
+
+
 async def __main__():
     config = load_config()
 
     queue = Queue[ScheduleEvent]()
-    tasks = set[TaskTracker]()
+    tasks = list[TaskTracker]()
 
     start_http_server(int(os.getenv("IMT_PROM_PORT", "8000")))
-    async with TaskGroup() as tg:
-        # all the prediction/schedule watchers run on the main thread using async awaits while
-        # the process_queue task runs on a separate thread to ensure updates continue in realtime
-        threading.Thread(target=process_queue, daemon=True, args=[queue]).start()
 
-        for stop in config.stops:
-            if stop.schedule_only:
-                task = tg.create_task(
-                    watch_static_schedule(
-                        stop.stop_id,
-                        stop.route_filter,
-                        stop.direction_filter,
-                        queue,
-                        stop.transit_time_min,
-                    )
-                )
-                tasks.add(TaskTracker(task=task, expiration_time=None, stop=stop))
-            else:
-                task = tg.create_task(
-                    watch_station(
-                        stop.stop_id,
-                        stop.route_filter,
-                        stop.direction_filter,
-                        queue,
-                        stop.transit_time_min,
-                    )
-                )
-                tasks.add(
-                    TaskTracker(
-                        task=task,
-                        expiration_time=datetime.now().astimezone(UTC)
-                        + timedelta(
-                            minutes=randint(
-                                MIN_TASK_RESTART_MINS, MAX_TASK_RESTART_MINS
-                            )
-                        ),
-                        stop=stop,
-                    )
-                )
+    threading.Thread(target=process_queue, daemon=True, args=[queue]).start()
+    for stop in config.stops:
+        if stop.schedule_only:
+            tasks.append(start_thread("schedule", stop, queue))
+        else:
+            tasks.append(start_thread("predictions", stop, queue))
 
-        while True:
-            await sleep(30)
-            for task in tasks:
-                if (
-                    task.expiration_time
-                    and datetime.now().astimezone(UTC) > task.expiration_time
-                ):
-                    try:
-                        logger.info(f"Restarting task for {task.stop}")
-                        # restart the task
-                        task.task.cancel()
-                        await task.task
-                    except CancelledError:
-                        new_task = tg.create_task(
-                            watch_station(
-                                task.stop.stop_id,
-                                task.stop.route_filter,
-                                task.stop.direction_filter,
-                                queue,
-                                task.stop.transit_time_min,
-                            )
-                        )
-                        tasks.remove(task)
-                        tasks.add(
-                            TaskTracker(
-                                task=new_task,
-                                expiration_time=datetime.now().astimezone(UTC)
-                                + timedelta(
-                                    minutes=randint(
-                                        MIN_TASK_RESTART_MINS, MAX_TASK_RESTART_MINS
-                                    )
-                                ),
-                                stop=task.stop,
-                            )
-                        )
+    while True:
+        await sleep(30)
+        for task in tasks:
+            if (
+                task.expiration_time
+                and datetime.now().astimezone(UTC) > task.expiration_time
+            ) or not task.task.is_alive():
+                tasks.remove(task)
+                tasks.append(start_thread("predictions", stop, queue))
 
 
 if __name__ == "__main__":
