abd79f6f826ebce44ce73a3fdf93fd17210a409f
Ryan <git@hexa.mozmail.com>
2025-01-18 04:09:17 -0500

    remove update time from git

--- inky-mbta-tracker/geojson_creator.py
+++ inky-mbta-tracker/geojson_creator.py
@@ -145,10 +145,7 @@ async def create_json(config: Config):
                                 "speed": vehicle_info.speed,
                                 "direction": vehicle_info.direction_id,
                                 "id": vehicle_info.id,
-                                "stop": vehicle_info.stop,
-                                "update_time": datetime.now()
-                                .astimezone(ZoneInfo("US/Eastern"))
-                                .strftime("%c"),
+                                "stop": vehicle_info.stop
                             },
                         )
                         features.append(feature)
