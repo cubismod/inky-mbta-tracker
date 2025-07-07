b925ecafaaf42f44b841a5c3b9604cb60c4fc2b6
Ryan <git@hexa.mozmail.com>
2025-07-06 23:10:07 -0400

    track vehicle speeds with prometheus

--- inky-mbta-tracker/prometheus.py
+++ inky-mbta-tracker/prometheus.py
@@ -8,6 +8,10 @@ vehicle_events = Counter(
     "imt_vehicle_events", "Any updates to vehicles", ["action", "route_id"]
 )
 
+vehicle_speeds = Gauge(
+    "imt_vehicle_speeds", "Vehicle speeds", ["route_id", "vehicle_id"]
+)
+
 tracker_executions = Counter("imt_tracker_executions", "Tracker Executions", ["stop"])
 
 mbta_api_requests = Gauge(
