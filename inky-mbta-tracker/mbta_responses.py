f47039c85b72a5a9ad7f41a958c59ceb65be9e80
Ryan <git@hexa.mozmail.com>
2025-07-10 18:48:35 -0400

    more optional values in alerts

--- inky-mbta-tracker/mbta_responses.py
+++ inky-mbta-tracker/mbta_responses.py
@@ -118,11 +118,11 @@ class ActivePeriod(BaseModel):
 class InformedEntity(BaseModel):
     trip: Optional[str] = None
     stop: Optional[str] = None
-    route_type: int
+    route_type: Optional[int] = None
     route: Optional[str] = None
     facility: Optional[str] = None
     direction_id: Optional[int] = None
-    activities: list[str]
+    activities: Optional[list[str]] = None
     image: Optional[str] = None
     service_effect: Optional[str] = None
     duration_certainty: Optional[str] = None
@@ -131,9 +131,9 @@ class InformedEntity(BaseModel):
 
 class AlertAttributes(BaseModel):
     timeframe: Optional[str] = None
-    image: Optional[str] = None
     image_alternative_text: Optional[str] = None
     cause: str
+    image: Optional[str] = None
     created_at: str
     banner: Optional[str] = None
     header: str
